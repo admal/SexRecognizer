c38434d6841239c3be60c5210c8ef5847361aced
Jędrzej Fijałkowski <jedrek@fijalkowscy.com>
2016-12-04 13:59:24 +0100

    silhouette extractor GPU implementation plus file saving naming improved

--- SexRecognizer.Common/FileManager.cpp
+++ SexRecognizer.Common/FileManager.cpp
@@ -20,7 +20,7 @@ void FileManager::SaveOuptut(string filepath, vector<cv::Mat> images)
 	for (int i = 0; i < images.size(); i++)
 	{
 		string file = filepath;
-		file.append(MakeFilename(maxLenght + 1, to_string(i + 1)));
+		file.append(MakeFilename(maxLenght, to_string(i)));
 		file.append(extension);
 
 		cv::imwrite(file, images[i]);
@@ -32,7 +32,7 @@ string FileManager::MakeFilename(int maxLength, std::string sufix)
 	std::ostringstream result;
 	result  <<
 		std::setfill('0') <<
-		std::setw(maxLength - sufix.length()) <<
+		std::setw(maxLength) <<
 		sufix;
 	return result.str();
 }
--- SexRecognizer.Detector/SilhouetteExtractor.cpp
+++ SexRecognizer.Detector/SilhouetteExtractor.cpp
@@ -18,7 +18,7 @@ SilhouetteExtractor::SilhouetteExtractor(int subtractionType){
 		this->subtractor = new Extract::BackgroundSubtractorMOG2();
 	}
 	//CPU
-	this->hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+	//this->hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
 	//GPU
 	Size win_size = Size(48, 96);
 	Size block_size = Size(16, 16);
@@ -88,23 +88,24 @@ std::vector<int> SilhouetteExtractor::findSilhouetteOffset(std::vector<cv::Mat> 
 			Rect r = found[i];
 			if (r.area() > max.area())
 				max = r;
-			if (i == found.size() - 1)
+			if (i == found.size() - 1) // when all rectangles compared the biggest is the only one left
 				found_filtered.push_back(max);
 		}
 		if (found.size() == 0 && firstDetected) // if no person found but it was detected previously we estimate the position and calc if it's still in frame
 		{
 			max = prevRect;
 			max.x += std::round(meanShift);
-			if (meanShift > 0){ // if moving right
+			if (meanShift > 0){ // if moving right TODO : here the person has to be checked to be in the middle of the image
 				if ((max.x + max.width) < imgWidth)
 					found_filtered.push_back(max);
 			} else if(meanShift < 0) // if moving left
 			{
-				if ((max.x - max.width) > 0)
+				if ((max.x) > 0)
 					found_filtered.push_back(max);
 			} else
 			{
 				v.push_back(-1);
+				outOfBounds = true;
 			}
 		}
 		if (found.size() == 0 && !firstDetected)
@@ -112,57 +113,55 @@ std::vector<int> SilhouetteExtractor::findSilhouetteOffset(std::vector<cv::Mat> 
 			v.push_back(-1);
 		}
 		
-		for (i = 0; i<found_filtered.size(); i++)  // TODO: found_filtered should always have 1 member so can be reduced to single var
+		for (i = 0; i < found_filtered.size(); i++)  // TODO: found_filtered should always have 1 member so can be reduced to single var
 		{
-
-			
+	
 			//CALCULATING SILHOUETTE FRAME OFFSET
 			// resizing to 80 x 60 and recalculating the x offset
 			int offset = found_filtered[0].x / imgScale;
 			int recalculatedOffeset = offset - ((60 - (found_filtered[0].width / imgScale)) / 2);
 
 			// CHECK OUTLIERS (HOG DETECTION ERRORS) and person OOB - for example when instead of a walking person it locks on a fire extinguisher
-			OUTLIERS_OOB:{
-				if (meanShift < 0 && v[v.size() - 1] < recalculatedOffeset) // if moving left and the frame suggests the next step is to right ignore it and calc from mean shift
-				{
-					// Take the previous offset and add a meanShift
-					recalculatedOffeset = v[v.size() - 1] + std::round(meanShift);
-				}
-				if (meanShift > 0 && v[v.size() - 1] > recalculatedOffeset) // similiar but the other way around
-				{
-					recalculatedOffeset = v[v.size() - 1] + std::round(meanShift);
-				}
-				if (recalculatedOffeset < 0 && meanShift < 0) { //if moving left gets OOB
-					outOfBounds = true; // if the frame was calculated to be outside of image for the person to be in the middle we stop the detection
-					break;
-				}
-				else if (recalculatedOffeset > 20 && meanShift > 0){ // if moving right gets OOB
-					outOfBounds = true;
-					break;
-				}
-				else
-				{
-					recalculatedOffeset = (recalculatedOffeset >= 0) ? recalculatedOffeset : 0;
-					recalculatedOffeset = (recalculatedOffeset + 60 <= 80) ? recalculatedOffeset : 20;
-				}
+			if (meanShift < 0 && v[v.size() - 1] < recalculatedOffeset) // if moving left and the frame suggests the next step is to right ignore it and calc from mean shift
+			{
+				// Take the previous offset and add a meanShift
+				recalculatedOffeset = v[v.size() - 1] + std::round(meanShift);
 			}
-			MEAN_SHIFT:{
-				if (firstDetected) // this will be entered when a second time person is detected
-				{
-					if (recalculatedOffeset == 20)
-					{
-						meanShift = -0.01;
-					} else if(recalculatedOffeset == 0){
-						meanShift = 0.01;
-					}else{ // the HOG algorithm went crazy or a person was recorded far from the edge
-						int change = max.x - prevRect.x;
-						if (change != 0) change > 0 ? meanShift = 0.01 : meanShift = -0.01;
-					}
-					meanShift = (double)((meanShift + calcShift(v[v.size() - 1], recalculatedOffeset)) / (double)shiftCounter);
-				}
-				prevRect = max;
-				firstDetected = true;
+			if (meanShift > 0 && v[v.size() - 1] > recalculatedOffeset) // similiar but the other way around
+			{
+				recalculatedOffeset = v[v.size() - 1] + std::round(meanShift);
 			}
+			if (recalculatedOffeset < 0 && meanShift < 0) { //if moving left gets OOB
+				outOfBounds = true; // if the frame was calculated to be outside of image for the person to be in the middle we stop the detection
+				break;
+			}
+			else if (recalculatedOffeset > 20 && meanShift > 0){ // if moving right gets OOB
+				outOfBounds = true;
+				break;
+			}
+			else
+			{
+				recalculatedOffeset = (recalculatedOffeset >= 0) ? recalculatedOffeset : 0;
+				recalculatedOffeset = (recalculatedOffeset + 60 <= 80) ? recalculatedOffeset : 20;
+			}
+			
+			// MEAN SHIFT
+			if (firstDetected) // this will be entered when a second time person is detected
+			{
+				if (recalculatedOffeset == 20)
+				{
+					meanShift = -0.01;
+				} else if(recalculatedOffeset == 0){
+					meanShift = 0.01;
+				}else{ // the HOG algorithm went crazy or a person was recorded far from the edge
+					int change = max.x - prevRect.x;
+					if (change != 0) change > 0 ? meanShift = 0.01 : meanShift = -0.01;
+				}
+				meanShift = (double)((meanShift + calcShift(v[v.size() - 1], recalculatedOffeset)) / (double)shiftCounter);
+			}
+			prevRect = max;
+			firstDetected = true;
+			
 			v.push_back(recalculatedOffeset);
 
 			Rect r = found_filtered[i];
@@ -178,13 +177,16 @@ std::vector<int> SilhouetteExtractor::findSilhouetteOffset(std::vector<cv::Mat> 
 		{
 			shiftCounter++;
 		}
-		namedWindow("a", 1);
-		imshow("a", frames[x]);
+		namedWindow("display", 1);
+		imshow("display", frames[x]);
 		waitKey(1);
-
-		//cv::imwrite("a"+to_string(x)+".png", frames[x]);
 	}
-
+	if (v.size() < frames.size()) // TODO : This shouldn't happen at this stage but w/e
+	{
+		for (int x = v.size() - 1; x < frames.size(); x++){
+			v.push_back(-1);
+		}
+	}
 	return v;
 }
 
@@ -194,14 +196,10 @@ std::vector<int> SilhouetteExtractor::findSilhouetteOffset(std::vector<cv::Mat> 
 std::vector<int> SilhouetteExtractor::extract(std::vector<cv::Mat> frames) {
 	//subtractedFrames = this->subtractor->subtract(frames);
 	Extract::ImageResizer resizer(160, 120);  // resize frames to better extract silhouette offset
-	std::vector<cv::Mat> resizedFrames = resizer.resizeFrames(frames);
-	std::vector<int> xSilhouetteOffsets = SilhouetteExtractor::findSilhouetteOffset(resizedFrames); // find a slhouette
+	std::vector<cv::Mat> resizedFramesForHOG = resizer.resizeFrames(frames);
+	std::vector<int> xSilhouetteOffsets = SilhouetteExtractor::findSilhouetteOffset(resizedFramesForHOG); // find a slhouette
 	resizer.setSize(Size(80, 60));  // resize frames to final 80 x 60 res
-<<<<<<< HEAD
-	resizedFrames = resizer.resizeFrames(resizedFrames);
-=======
 	this->resizedFrames = resizer.resizeFrames(frames);
->>>>>>> f29a153f9f578529a44cfd9f33e1de2c88d71c9c
 	return xSilhouetteOffsets;
 }
 

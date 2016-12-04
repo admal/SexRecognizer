ceb506dbac29df6cbc01e28f620d19989c2bb6ba
admal <adam.malewski.wp.pl>
2016-12-04 22:37:34 +0100

    fixy skrypt

--- generate_OF_lbl_frames.ps1
+++ generate_OF_lbl_frames.ps1
@@ -2,7 +2,7 @@ $programPath="C:\Users\Adam\Documents\Visual Studio 2013\Projects\SexRecognizer.
 $inputPath = "C:\Users\Adam\Pictures\inz-klatki-high\test"
 $outputPath = "C:\Users\Adam\Pictures\inz-klatki-high\test_output\"
 $labelsPath = "C:\Users\Adam\Pictures\inz-klatki-high\tumgaid_labels\allgender.txt"
-$labelsOutputPath = "C:\Users\Adam\Pictures\inz-klatki-high\test\labels.txt"
+$labelsOutputPath = "C:\Users\Adam\Pictures\inz-klatki-high\test_output\labels.txt"
 $labels = Get-Content $labelsPath
 $personDirs = Get-ChildItem $inputPath | where {$_.Attributes -eq 'Directory'} | % { $_.FullName }
 
@@ -15,17 +15,31 @@ $labelFileContent = ""
 foreach($pdir in $personDirs)
 {
 	$seqDirs = Get-ChildItem $pdir | where {$_.Attributes -eq 'Directory'} | % { $_.FullName }
+    $simpleDirs = Get-ChildItem $pdir | where {$_.Attributes -eq 'Directory'}
+
+    $index = 0;
+
 	foreach($sdir in $seqDirs)
 	{
-        write-host $sdir
-        $arg1 = $sdir
-        $arg2 = "-p"
-        $arg3 = [io.path]::combine($outputPath, [string]$i)
-        if ($arg1.StartsWith("Script"))
-        New-Item $arg3 -type directory
-		& $programPath $arg1 $arg2 $arg3
-        $i = $i + 1
-        $labelFileContent+= ([string]$i+';'+[string](($labels[$j] -as [int]) - 1) + '`n')
+        $simpleDirName = [string]$simpleDirs[$index]
+        Write-Host simple
+        Write-Host $simpleDirName
+
+        if ($simpleDirName.StartsWith("n"))
+        {
+            Write-Host SDIR
+            write-host $sdir
+            $arg1 = $sdir
+            $arg2 = "-p"
+            $arg3 = [io.path]::combine($outputPath, [string]$i)
+        
+            New-Item $arg3 -type directory
+		    & $programPath $arg1 $arg2 $arg3
+            $i = $i + 1
+            $labelFileContent+= ([string]$i+';'+[string](($labels[$j] -as [int]) - 1) +  "`n")
+        }
+        $index = $index + 1
+        
 	}
     $j = $j + 1
 }
